e447e36d5202dbad1d1e270646367da1309cd727
shinespark <shinespark64@gmail.com>
2017-11-23 01:30:25 +0900

    Complete tutorial05

--- django-tutorial/django_tutorial/settings.py
+++ django-tutorial/django_tutorial/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sessions',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
